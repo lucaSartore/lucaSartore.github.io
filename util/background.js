27c91dbfde1a58d84f39649ae7fea83d82ea397d
lucaSartore <lucasar.com8@gmail.com>
2023-10-22 13:14:29 +0200

    fix bg in secondary pages

--- util/background.js
+++ util/background.js
@@ -3,8 +3,5 @@ const bg = document.getElementById('background');
 document.addEventListener('mousemove', function(e) {
     const x = e.clientX;
     const y = e.clientY;
-
-    console.log(x, y);
-
     bg.style.backgroundPosition = `${x}px ${y}px`;
 });
