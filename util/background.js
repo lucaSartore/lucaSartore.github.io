0a8f9ffa77f2bca0cfb323a16501de5e727a7ac0
lucaSartore <lucasar.com8@gmail.com>
2025-01-04 14:48:32 +0100

    remove all old files

--- util/background.js
+++
@@ -1,7 +0,0 @@
-const bg = document.getElementById('background');
-
-document.addEventListener('mousemove', function(e) {
-    const x = e.clientX;
-    const y = e.clientY;
-    bg.style.backgroundPosition = `${x}px ${y}px`;
-});
