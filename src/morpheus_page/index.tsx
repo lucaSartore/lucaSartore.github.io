dd790184bd970b344678829124ac527f43f1702e
lucaSartore <lucasar.com8@gmail.com>
2025-01-04 18:16:00 +0100

    Add blue and red pill animations

--- src/morpheus_page/index.tsx
+++ src/morpheus_page/index.tsx
@@ -1,12 +1,25 @@
 import image from '../assets/morpheus.jpg'
+import blue_pill_border from '../assets/blue_pill_border.png'
+import red_pill_border from '../assets/red_pill_border.png'
 import "./index.css"
 
 function MorpheusPage() {
 
+    
+            // <img className='image' id="blue_pill_border" style={{ filter: "drop-shadow(0 0 10px #0000ff)" }} src={blue_pill_border}   />
+            // <img className='image' id="red_pill_border" style={{ filter: "drop-shadow(0 0 10px #ff0000)" }} src={red_pill_border}   />
+
+
   return (
     <>
-      <div className='main_div'>
-        <img className='main_img' src={image}   />
+      <div id='main_div'>
+        <div id='img_div'>
+            <div id="red_pill_div"></div>
+            <div id="blue_pill_div"></div>
+            <img className='image' src={image}   />
+            <img className='image' id="blue_pill_border"  src={blue_pill_border}   />
+            <img className='image' id="red_pill_border"  src={red_pill_border}   />
+        </div>
       </div>
     </>
   )
