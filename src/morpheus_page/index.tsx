3444d65e96fc4d9960277fd7ec4e7a7924a1ab1d
lucaSartore <lucasar.com8@gmail.com>
2025-01-04 16:48:26 +0100

    Add morpheus page

---
+++ src/morpheus_page/index.tsx
@@ -0,0 +1,15 @@
+import image from '../assets/morpheus.jpg'
+import "./index.css"
+
+function MorpheusPage() {
+
+  return (
+    <>
+      <div className='main_div'>
+        <img className='main_img' src={image}   />
+      </div>
+    </>
+  )
+}
+
+export default MorpheusPage
