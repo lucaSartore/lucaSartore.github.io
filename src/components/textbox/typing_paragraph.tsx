532d004e7bd6a0c1a57d364e7a22d20fdc1e5d2b
lucaSartore <lucasar.com8@gmail.com>
2025-01-05 14:54:51 +0100

    Finish adding pillow descriptions

--- src/components/textbox/typing_paragraph.tsx
+++ src/components/textbox/typing_paragraph.tsx
@@ -5,7 +5,7 @@ import React, {
 } from "react";
 import  { TypingTextProps, TypingTextRef, TypingText } from "./typing_text";
 
-const TYPING_PERIOD_MS = 5;
+const DEFAULT_TYPING_TIME_MS = 1200;
 const TYPING_VOLUME = 0.2;
 
 
@@ -13,11 +13,12 @@ const TYPING_VOLUME = 0.2;
 export type TypingParagraphProps = {
     items: Array<TypingTextProps | JSX.Element>
     className: string | undefined
+    typingTime: number | undefined
 }
 
 export type TypingParagraphRef = {
-    start: (callback: () => void) => void;
-    stop: (callback: () => void) => void;
+    start: () => void;
+    stop: () => void;
 }
 
 
@@ -25,6 +26,7 @@ export const TypingParagraph = forwardRef((props: TypingParagraphProps, ref: Rea
     
     const className = props.className || "";
     const items = props.items;
+    const typingTime = props.typingTime || DEFAULT_TYPING_TIME_MS
 
 	const audioRef: React.MutableRefObject<HTMLAudioElement | null> = useRef(null);
 
@@ -32,17 +34,29 @@ export const TypingParagraph = forwardRef((props: TypingParagraphProps, ref: Rea
     // creating all sub objects
     let items_refs: Array<React.RefObject<TypingTextRef>> = []
     let items_html: Array<JSX.Element> = []
-    items.forEach(item => {
+    
+    let numLetters = 0;
+
+    items.forEach((item, i) => {
         if ("className" in item && "text" in item){
             const ref = useRef<TypingTextRef>(null);
+            numLetters += item.text?.length || 0;
             items_html.push(
-                <TypingText ref={ref} className={item.className} text = {item.text} />
+                <TypingText ref={ref} className={item.className} text = {item.text} key={i}/>
             )
             items_refs.push(ref);
         }else{
+            item.key = i.toString();
             items_html.push(item)
         }
     });
+    
+    let typingInterval = typingTime / numLetters;
+    let scalingFactor = 1;
+    if (typingInterval < 60) {
+        scalingFactor = Math.ceil(60 / typingInterval);
+        typingInterval = typingTime / (numLetters/scalingFactor);
+    }
 
 	const startAudio = function () {
 		if (audioRef.current) {
@@ -75,22 +89,25 @@ export const TypingParagraph = forwardRef((props: TypingParagraphProps, ref: Rea
     const innerTextIndex = useRef(0);
 
      
-    function textUpdateHandler(callback: () => void){
+    function textUpdateHandler(){
         let has_finish_updates = false;
         if (status.current == "TYPING"){
-            //@ts-expect-error
-            has_finish_updates = items_refs[innerTextIndex.current].current.forward();
+            for (let i=0; i<scalingFactor; i++){
+                //@ts-expect-error
+                has_finish_updates = items_refs[innerTextIndex.current].current.forward();
+            }
         }else{
-            //@ts-expect-error
-            has_finish_updates = items_refs[innerTextIndex.current].current.backword();
+            for (let i=0; i<scalingFactor; i++){
+                //@ts-expect-error
+                has_finish_updates = items_refs[innerTextIndex.current].current.backword();
+            }
         }
 
         if (has_finish_updates && status.current == "DELETING" && innerTextIndex.current == 0 ||
-            has_finish_updates && status.current == "DELETING" && innerTextIndex.current == items_refs.length
+            has_finish_updates && status.current == "TYPING" && innerTextIndex.current+1 == items_refs.length
            ){
             stopAudio();
             isUpdaterRunning.current = false;
-            callback();
             return;
         }
 
@@ -101,27 +118,27 @@ export const TypingParagraph = forwardRef((props: TypingParagraphProps, ref: Rea
             innerTextIndex.current += 1;
         }
 
-		setTimeout(textUpdateHandler, TYPING_PERIOD_MS);
+		setTimeout(textUpdateHandler, typingInterval);
     }
 
-    function startTextUpdater(callback: () => void){
+    function startTextUpdater(){
         if ( isUpdaterRunning.current ){
             return
         }
         startAudio();
         isUpdaterRunning.current = true
-        textUpdateHandler(callback);
+        textUpdateHandler();
     }
 
 
 	useImperativeHandle(ref, () => ({
-		start: (callback: () => void) => {
+		start: () => {
             status.current = "TYPING";
-            startTextUpdater(callback);
+            startTextUpdater();
 		},
-		stop: (callback: () => void) => {
+		stop: () => {
             status.current = "DELETING";
-            startTextUpdater(callback);
+            startTextUpdater();
 		},
 	}));
 
--- src/morpheus_page/index.tsx
+++ src/morpheus_page/index.tsx
@@ -2,12 +2,12 @@ import image from "../assets/morpheus.jpg";
 import blue_pill_border from "../assets/blue_pill_border.png";
 import red_pill_border from "../assets/red_pill_border.png";
 import "./index.css";
-import {TypingParagraph, TypingParagraphRef} from "../components/textbox";
+import { TypingParagraph, TypingParagraphRef } from "../components/textbox";
 import { useRef } from "react";
 
 function MorpheusPage() {
-
-	const ref = useRef<TypingParagraphRef>(null);
+	const refRedText = useRef<TypingParagraphRef>(null);
+	const refBlueText = useRef<TypingParagraphRef>(null);
 
 	return (
 		<>
@@ -15,23 +15,59 @@ function MorpheusPage() {
 				<div id="img_div">
 					<div
 						id="red_pill_div"
-						onMouseEnter={() => ref.current?.start(() => {})}
-						onMouseLeave={() => ref.current?.stop(() => {})}
+						onMouseEnter={() => refRedText.current?.start()}
+						onMouseLeave={() => refRedText.current?.stop()}
 					></div>
-					<div id="blue_pill_div"></div>
+					<div
+						id="blue_pill_div"
+						onMouseEnter={() => refBlueText.current?.start()}
+						onMouseLeave={() => refBlueText.current?.stop()}
+					></div>
 					<img className="image" src={image} />
 					<img className="image" id="blue_pill_border" src={blue_pill_border} />
 					<img className="image" id="red_pill_border" src={red_pill_border} />
-					<TypingParagraph ref={ref} className="red_pill_text_box" items={[
-                        {
-                            className: "",
-                            text: "title\n"
-                        },
-                        {
-                            className: "",
-                            text: "text text text"
-                        }
-                    ]} />
+					<TypingParagraph
+						ref={refRedText}
+						className="red_pill_text_box"
+                        typingTime={1000}
+						items={[
+							{
+								className: "",
+								text: "Let’s have some fun!",
+							},
+							{
+								className: "",
+								text: `Explore my personal “Website-Game”... In order to get any information about me you will have to fight!`,
+							},
+							{
+								className: "",
+								text: `Explore an abandoned city and try to survive while hunted by a group of killer robots. If you manage to defeat them, and reach an “Information point” you will be rewarded with the ultimate treasure! (aka my contact info, my cv, my portfolio etcetera).`,
+							},
+							{
+								className: "",
+								text: `Remember that if you change your mind you can always refresh the website and choose the blue pill.`, },
+						]}
+					/>
+
+					<TypingParagraph
+						ref={refBlueText}
+						className="blue_pill_text_box"
+                        typingTime={1000}
+						items={[
+							{
+								className: "",
+								text: "Go straight to the point!",
+							},
+							{
+								className: "",
+								text: `You don’t have time to waste don’t you? I see, in this case you can choose the blue pill, and you will be able to immediately get what you are looking for.`,
+							},
+							{
+								className: "",
+								text: `Remember that if you change your mind you can always refresh the website and choose the red pill.`,
+							},
+						]}
+					/>
 				</div>
 			</div>
 		</>
