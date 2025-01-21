2d7674cd6172e3ce6952d48d0b26d5afc1ca90d2
Luca Sartore <lucasar.com8@gmail.com>
2025-01-21 17:44:38 +0100

    Make slide in animation

--- src/components/pda/index.tsx
+++ src/components/pda/index.tsx
@@ -1,12 +1,16 @@
 import pda_img from "../../assets/pda.png";
+import desk_img from "../../assets/desk_3.jpg";
 import "./index.css"
 
 
 function PdaPage(){
     return (
 		<>
-            <div className="pda-page-main-div">
-                <PdaWrapper/>               
+            <div id="pda-page-main-div">
+                <div id="image-container">
+                    <PdaWrapper/>               
+                </div>
+                <img src={desk_img} id="desk-img"/>
             </div>
 		</>
     )
