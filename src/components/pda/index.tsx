848b550fc99538b09d81bed976746d2f05e476ae
Luca Sartore <lucasar.com8@gmail.com>
2025-01-22 08:50:47 +0100

    make pda screen div follow the image

--- src/components/pda/index.tsx
+++ src/components/pda/index.tsx
@@ -1,5 +1,6 @@
 import pda_img from "../../assets/pda.png";
 import desk_img from "../../assets/desk_3.jpg";
+import React, { useEffect, useState, useRef } from "react";
 import "./index.css"
 
 
@@ -7,7 +8,7 @@ function PdaPage(){
     return (
 		<>
             <div id="pda-page-main-div">
-                <div id="image-container">
+                <div id="pda-container">
                     <PdaWrapper/>               
                 </div>
                 <img src={desk_img} id="desk-img"/>
@@ -17,11 +18,51 @@ function PdaPage(){
 }
 
 function PdaWrapper() {
-	return (
-		<>
-            <img src={pda_img} className="pda-class"/>
-		</>
-	);
+    const [overlayStyle, setOverlayStyle] = useState({});
+    const imageRef = useRef<HTMLImageElement>(null);
+
+    useEffect(() => {
+        const updateOverlay = () => {
+            const image = imageRef.current;
+            if (!image) return;
+
+            const imageRect = image.getBoundingClientRect();
+            const imageWidth = imageRect.width;
+            const imageHeight = imageRect.height;
+
+            const topLeftX = imageWidth * 0.264;
+            const topLeftY = imageHeight * 0.115;
+            const bottomRightX = imageWidth * 0.975;
+            const bottomRightY = imageHeight * 0.963;
+
+            const overlayWidth = bottomRightX - topLeftX;
+            const overlayHeight = bottomRightY - topLeftY;
+
+            setOverlayStyle({
+                width: `${overlayWidth}px`,
+                height: `${overlayHeight}px`,
+                left: `${imageRect.left + topLeftX}px`,
+                top: `${imageRect.top + topLeftY}px`,
+            });
+        };
+
+        updateOverlay();
+        window.addEventListener("resize", updateOverlay);
+
+        return () => window.removeEventListener("resize", updateOverlay);
+    }, []);
+
+    return (
+        <>
+            <img
+                ref={imageRef}
+                src={pda_img}
+                className="pda-class"
+                alt="PDA"
+            />
+            <div className="pda-screen" style={overlayStyle}></div>
+        </>
+    );
 }
 
 export default PdaPage;
