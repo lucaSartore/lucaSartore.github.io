e9b6428d170e53eda3aa9d996193eaeda25fa962
Luca Sartore <lucasar.com8@gmail.com>
2025-01-22 11:11:50 +0100

    Add color theme structure

--- src/components/pda/index.tsx
+++ src/components/pda/index.tsx
@@ -1,8 +1,9 @@
 import pda_img from "../../assets/pda.png";
 import desk_img from "../../assets/desk_3.jpg";
 import "./index.css"
+import "./themes.css"
 import { forwardRef, PropsWithChildren, useImperativeHandle, useRef } from "react";
-
+import { ThemeProvider } from "./theme_provider";
 
 function PdaPage() {
     return (
@@ -12,6 +13,92 @@ function PdaPage() {
                     <PdaWrapper >
                         <h1> hello </h1>
                         <a> this is some cool text inside my pda </a>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        <h1> hello </h1>
+                        
                     </PdaWrapper>
                 </div>
                 <img src={desk_img} id="desk-img" />
@@ -42,8 +129,13 @@ const PdaWrapper = forwardRef((props: PropsWithChildren, ref) => {
                     className="pda-img"
                     alt="PDA"
                 />
-                <div className="pda-screen" >
-                    {props.children}
+                <div className="pda-screen themed-component" >
+                </div>
+
+                <div className="pda-screen-top themed-component">
+                    <ThemeProvider>
+                        {props.children}
+                    </ThemeProvider>
                 </div>
             </div>
         </>
---
+++ src/components/pda/theme_provider.tsx
@@ -0,0 +1,41 @@
+import  { createContext, useContext, useState, useEffect, PropsWithChildren} from 'react';
+
+type Theme = "one-dark" | "paper-like" | "hacker";
+function isTheme(value: any): value is Theme {
+    return ["one-dark", "paper-like", "hacker"].includes(value);
+}
+
+const ThemeContext = createContext<{
+    theme: Theme,
+    toggleTheme: (_: Theme) => void
+}>({
+    theme: "one-dark",
+    toggleTheme: _ => {}
+});
+
+
+export const ThemeProvider = (props: PropsWithChildren) => {
+
+  const [theme, setTheme] = useState(() => {
+    var savedTheme = localStorage.getItem('theme');
+    if (isTheme(savedTheme)){
+        return savedTheme
+    }
+    return "one-dark"
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = (newTheme: Theme) => {
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
