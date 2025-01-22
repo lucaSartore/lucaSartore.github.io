f8adcab2b122da5b7c0e1a20eeb71ea90b2fa6fe
Luca Sartore <lucasar.com8@gmail.com>
2025-01-22 19:21:45 +0100

    Add url in navigation bar

--- src/components/pda/navigation_bar.tsx
+++ src/components/pda/navigation_bar.tsx
@@ -1,14 +1,20 @@
 import "./navigation_bar.css"
-
 import Icon from '@mdi/react';
-import { mdiFullscreen, mdiCloseOctagonOutline, mdiArrowLeftCircleOutline} from '@mdi/js';
+import { mdiFullscreen, mdiCloseOctagonOutline, mdiArrowLeftCircleOutline, mdiCog} from '@mdi/js';
+import { useLocation } from 'react-router';
+import "../../fonts.css"
 
 
 export function NavigationBar(){
+    const location = useLocation();
+    const pathName = location.pathname.slice(1).replace("/", " > ") + "foo> foo> foo> foo> ";
+
     return (
         <>
             <div className="navigation-bar">
+                <h1 className="url-value custom-font"> {pathName} </h1>
                 <Icon path={mdiArrowLeftCircleOutline} style={{marginLeft: "auto"}}  className="icon" />
+                <Icon path={mdiCog}  className="icon" />
                 <Icon path={mdiFullscreen}  className="icon" />
                 <Icon path={mdiCloseOctagonOutline}  style={{marginRight: "1%"}} className="icon" />
             </div>
